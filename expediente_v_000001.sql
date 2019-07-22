2a0e654a441de1a013543339bbb537a976aac4a6
Verny Mata <vernym@gmail.com>
2019-07-22 13:33:24 -0600

    Carga inicial de script de creación de objetos

---
+++ expediente_v_000001.sql
@@ -0,0 +1,5 @@
+﻿create table hermanas (cedula int(15) not null, nombre varchar(20), primer_apellido varchar(20), segundo_apellido varchar(20));
+insert into hermanas (cedula, nombre, primer_apellido, segundo_apellido) values (1,"Ismaela","Arce","Arce");
+DELIMITER // 
+create procedure prueba (OUT pError varchar(100)) begin select cedula, nombre, primer_apellido, segundo_apellido from hermanas; select "Exitos Totales" into pError; end// 
+DELIMITER ;
